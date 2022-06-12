25612fc6deb75cc74812c448ece66de858f11fba
TranLinh3 <105622353+TranLinh3@users.noreply.github.com>
2022-06-12 22:22:35 +0700

    Update TransactionHistory.java

--- bank/TransactionHistory.java
+++ bank/TransactionHistory.java
@@ -1,33 +1,60 @@
-package bank;
+package Bank;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-public class TransactionHistory  {
+
+public class TransactionHistory {
     private Long id;
     private LocalDate dayTrading;
-    private String depscription;
+    private String description;
     private String beneficiaryAccount;
     private long money;
-    public TransactionHistory(String depscription, String beneficiaryAccount, long money){
+
+    public TransactionHistory(String description, String beneficiaryAccount, long money) {
         this.id = IdGenerate.getNewID();
-        //id để tự động tăng
         this.dayTrading = LocalDate.now();
-        //lấy ngày hiện tại
-        this.depscription = depscription;
+        this.description = description;
         this.beneficiaryAccount = beneficiaryAccount;
         this.money = money;
     }
-    public String getBeneficiaryAccount(){ return beneficiaryAccount;
-    }
-    public void setBeneficiaryAccount(String beneficiaryAccount){this.beneficiaryAccount = beneficiaryAccount;}
-    public Long getId(){return id;
-    }
-    public LocalDate getDescription(){return depscription;}
-    public void setDepscription(String depscription){this.depscription = depscription;}
-    public long getMoney(){return money;}
-    public void setMoney(long money){this.money = money;}
-    public String toString(){
-        DateTimeFormatter myFomatter = DateTimeFormatter.ofPattern("dd/MM/YYYY");
-        return id + " - " + dayTrading.format(myFomatter) + " - " + depscription + " - " + Controller.formatMoney(money) + " - " + beneficiaryAccount + "\n";
 
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getDayTrading() {
+        return dayTrading;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBeneficiaryAccount() {
+        return beneficiaryAccount;
+    }
+
+    public void setBeneficiaryAccount(String beneficiaryAccount) {
+        this.beneficiaryAccount = beneficiaryAccount;
+    }
+
+    public long getMoney() {
+        return money;
+    }
+
+    public void setMoney(long money) {
+        this.money = money;
+    }
+
+    public String toString() {
+        DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return id + " - " + dayTrading.format(myFormatter) + " - " + description + " - " + Controller.formatMoney(money) + " - " + beneficiaryAccount + "\n";
+    }
+    public void printHistory() {
+        System.out.printf("%d | %s | %s | %s | %d\n ", getId(), getDayTrading(), getDescription(), getBeneficiaryAccount(), getMoney());
     }
 }
