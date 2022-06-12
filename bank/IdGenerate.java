8923b859b82c254a626c5b5657092c0b57705e37
TranLinh3 <105622353+TranLinh3@users.noreply.github.com>
2022-06-12 22:21:51 +0700

    Update IdGenerate.java

--- bank/IdGenerate.java
+++ bank/IdGenerate.java
@@ -1,24 +1,28 @@
-package bank;
+package Bank;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
+
 public class IdGenerate {
-    private static Long id = 0L;
-    public static ReentrantLock lock = new ReentrantLock();
+
+
+    private static Long id = 0l;
+    private static ReentrantLock lock = new ReentrantLock();
+
     public static Long getNewID(){
-        Long result;
+        long result;
         lock.lock();
-        //khóa biến id lại chỉ cho duy nhất 1 thread được truy xuất cập
-        //để tránh tình trạng Data Racing
-        try {
-            result = ++ id;
-            //tăng rồi gán vào result
-        }
-        finally {
+
+        try{
+            result = ++id;
+        } finally {
             lock.unlock();
-            //tăng rồi thì mở lock cho thread khác cùng truy cập
-            //nhiều như mình dùng xong thì đi ra
         }
         return result;
     }
-    private IdGenerate(){
-    }
+
+
+
 }
