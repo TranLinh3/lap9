69faad6c8bdae0fd46db018591ffe96fa11be531
TranLinh3 <105622353+TranLinh3@users.noreply.github.com>
2022-06-12 22:19:42 +0700

    Update Application.java

--- bank/Application.java
+++ bank/Application.java
@@ -1,28 +1,32 @@
-package bank;
+package Bank;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        ValidateAccount validate= new ValidateAccount();
+        ValidateAccount validate = new ValidateAccount();
         Controller controller = new Controller();
 
+        
         boolean isCheck = false;
-        while (!isCheck) {
-            System.out.println("Nhap so dien thoai");
+        while (!isCheck){
+            System.out.println("Nhập số điện thoại: ");
             String mobile = sc.nextLine();
-            System.out.println("Nhap mat khau: ");
-            String password =sc.nextLine();
 
-            isCheck=validate.checkAccount(mobile,password);
+            System.out.println("Nhập mật khẩu: ");
+            String password = sc.nextLine();
+            
+            isCheck = validate.checkAccount(mobile, password);
         }
         while (true) {
             menu();
-            int choose =sc.nextInt();
+            int choose = sc.nextInt();
             switch (choose) {
                 case 1:
-                    System.out.println("So du ");
+                    System.out.println("Số dư: ");
                     long balance = controller.getBalanceNumber();
                     System.out.println(Controller.formatMoney(balance));
                     break;
@@ -30,22 +34,27 @@ public class Application {
                     controller.actionTransfer();
                     break;
                 case 3:
-                    System.out.println("Lich su giao dich: ");
+                    System.out.println("Lịch sử giao dịch: ");
+                    controller.getHistory();
+                    break;
                 case 0:
                     sc.close();
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("Khong co lua chon nay");
+                    System.out.println("Không có lựa chọn này");
                     break;
             }
         }
     }
     public static void menu() {
-        System.out.println("Lua hon cua ban: ");
-        System.out.println("1. Xem so du tai khoan");
-        System.out.println("2. Chuyen khoan");
-        System.out.println("3.Xem lich su giao dich");
-        System.out.println("0. Thoat");
+        System.out.println("1. Xem số dư tài khoản");
+        System.out.println("2. Chuyển khoản");
+        System.out.println("3. Xem lịch sử giao dịch");
+        System.out.println("0. Thoát");
+        System.out.println("Lựa chọn của bạn: ");
     }
+
+
+
 }
